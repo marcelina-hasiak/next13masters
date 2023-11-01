1fa0979f9093b33f379eec4a44c78613f96bfeff
Marcelina <marcelina.hasiak@gmail.com>
2023-11-01 12:26:02 +0100

    feat: define fonts

---
+++ src/app/fonts.ts
@@ -0,0 +1,23 @@
+import {
+	Orbitron,
+	Sarpanch,
+	Share_Tech_Mono,
+} from "next/font/google";
+
+export const shareTechMono = Share_Tech_Mono({
+	weight: ["400"],
+	subsets: ["latin"],
+	display: "swap",
+});
+
+export const orbitron = Orbitron({
+	weight: ["500"],
+	subsets: ["latin"],
+	display: "swap",
+});
+
+export const sarpanch = Sarpanch({
+	weight: ["700"],
+	subsets: ["latin"],
+	display: "swap",
+});
