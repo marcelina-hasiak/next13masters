a14fc3e8f0499c41f3e73e45bbe8d275f0498d49
Marcelina <marcelina.hasiak@gmail.com>
2023-11-19 20:19:10 +0100

    feat: add new styling and product images

--- src/components/atoms/ProductImage.tsx
+++ src/components/atoms/ProductImage.tsx
@@ -6,13 +6,13 @@ export const ProductImage = ({
 	src: string;
 }) => {
 	return (
-		<div className="mb-4 overflow-hidden">
+		<div className="mb-4 overflow-hidden rounded">
 			<img
 				width={200}
 				height={350}
 				alt={alt}
 				src={src}
-				className="rounded transition-transform hover:scale-150 xxsm:w-full"
+				className="3xsm:w-full transition-transform hover:scale-150"
 			/>
 		</div>
 	);
